aa0e3779b43b2f514ed9b7fe6a56d5cfe47cfcca
Armagan Amcalar <armagan@amcalar.com>
2018-04-17 23:00:11 +0200

    Fix indentation and if / else template problem in person component template

--- frontend/src/components/person.vue
+++ frontend/src/components/person.vue
@@ -1,21 +1,20 @@
 <template lang="pug">
 .person-list
   h1 Hello!
-    div(v-if="people.length")
-      p Here are the people:
-      table
-        thead
-          td ID
-          td Name
-          td Age
-          tr(v-for="person in people")
-            td {{person.id}}
-            td {{person.name}}
-            td {{person.age}}
-
-      pre= JSON.stringify(people, null, 2)
-    p(v-else) There's no one yet :(
-
+  div(v-if="people.length")
+    p Here are the people:
+    table
+      thead
+        td ID
+        td Name
+        td Age
+        tr(v-for="person in people")
+          td {{person.id}}
+          td {{person.name}}
+          td {{person.age}}
+    pre= JSON.stringify(people, null, 2)
+  div(v-else)
+    p There's no one yet :(
 </template>
 
 <script>
